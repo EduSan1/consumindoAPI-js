1eaa9b83155b0ecce01060c2ba82a03e6e1c41e4
EDUARDO SANTOS NASCIMENTO <21276323@CFP127ALUNO.LOCAL>
2022-03-09 16:47:32 -0300

    1.1 - DOGS

---
+++ dog/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const pesquisar = document.getElementById("pesquisar");
+
+const pesquisarImagem = async (raca) => {
+  const url = `https://dog.ceo/api/breed/${raca}/images`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const carregarImagens = async () => {
+  const container = document.getElementById("imagem-container");
+  const raca = document.getElementById("raca").value;
+  const imagens = await pesquisarImagem(raca);
+
+  const imagem = imagens.message.map(criarImagem);
+  container.replaceChildren(...imagem);
+};
+
+const criarImagem = (imagem) => {
+  const img = document.createElement("img");
+  img.src = imagem;
+  img.classList.add("imagem");
+  return img;
+};
+pesquisar.addEventListener("click", carregarImagens);
